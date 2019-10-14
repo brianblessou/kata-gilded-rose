01f103d77f0104cdca5e0db6a558d5605bb41012
robin <robin.cavalieri@businessdecision.be>
2019-10-14 14:19:36 +0200

    On if per object

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -21,22 +21,40 @@ class GildedRose(val items: Array[Item]) {
           if (item.quality > 0) {
             item.quality -= 1
           }
+          if (item.sellIn < 0) {
+            if (item.quality > 0) {
+              item.quality -= 1
+            }
+          }
         }
+
         if (item.name.equals("Elixir of the Mongoose")) {
           if (item.quality > 0) {
             item.quality -= 1
           }
+          if (item.sellIn < 0) {
+            if (item.quality > 0) {
+              item.quality -= 1
+            }
+          }
         }
+
         if (item.name.equals("Conjured Mana Cake")) {
           if (item.quality > 0) {
             item.quality -= 1
           }
+          if (item.sellIn < 0) {
+            if (item.quality > 0) {
+              item.quality -= 1
+            }
+          }
         }
+
         if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
           if (item.quality < 50) {
             item.quality += 1
             if (item.quality < 50) {
-              if (item.sellIn < 10){
+              if (item.sellIn < 10) {
                 item.quality += 1
               }
               if (item.sellIn < 5) {
@@ -44,29 +62,18 @@ class GildedRose(val items: Array[Item]) {
               }
             }
           }
+          if (item.sellIn < 0) {
+            item.quality = 0
+          }
         }
+
         if (item.name.equals("Aged Brie")) {
           if (item.quality < 50) {
             item.quality += 1
           }
           if (item.sellIn < 0) {
-            if (item.name.equals("Aged Brie")) {
-              if (item.quality < 50) {
-                item.quality += 1
-              }
-            }
-          }
-        }
-
-        if (item.sellIn < 0) {
-          if (item.name.equals("+5 Dexterity Vest") || item.name.equals("Elixir of the Mongoose") || item.name.equals("Conjured Mana Cake") || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.name.equals("+5 Dexterity Vest") || item.name.equals("Elixir of the Mongoose") || item.name.equals("Conjured Mana Cake")) {
-              if (item.quality > 0) {
-                  item.quality -= 1
-              }
-            }
-            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-              item.quality = 0
+            if (item.quality < 50) {
+              item.quality += 1
             }
           }
         }
