68d2fd458df2a041b5327b4ffe758729dfb197c3
robin <robin.cavalieri@businessdecision.be>
2019-10-14 14:26:48 +0200

    Increase / Decrease func

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -4,6 +4,45 @@ import util.control.Breaks._
 
 class GildedRose(val items: Array[Item]) {
 
+  /**
+   *
+   * @param quality
+   * @return
+   */
+  def increaseQuality(quality: Int): Int = {
+    quality + 1
+  }
+
+  /**
+   *
+   * @param quality
+   * @return
+   */
+  def decreaseQuality(quality: Int): Int = {
+    quality - 1
+  }
+
+  /**
+   *
+   * @param sellIn
+   * @return
+   */
+  def increaseSellIn(sellIn: Int): Int = {
+    sellIn + 1
+  }
+
+  /**
+   *
+   * @param sellIn
+   * @return
+   */
+  def decreaseSellIn(sellIn: Int): Int = {
+    sellIn - 1
+  }
+
+  /**
+   *
+   */
   def updateQuality() {
 
     for (item <- items) {
@@ -14,51 +53,51 @@ class GildedRose(val items: Array[Item]) {
       }
       else {
 
-        item.sellIn -= 1
+        item.sellIn = decreaseSellIn(item.sellIn)
 
         // ITEM QUALITY MANAGEMENT
         if (item.name.equals("+5 Dexterity Vest")) {
           if (item.quality > 0) {
-            item.quality -= 1
+            item.quality = decreaseQuality(item.quality)
           }
           if (item.sellIn < 0) {
             if (item.quality > 0) {
-              item.quality -= 1
+              item.quality = decreaseQuality(item.quality)
             }
           }
         }
 
         if (item.name.equals("Elixir of the Mongoose")) {
           if (item.quality > 0) {
-            item.quality -= 1
+            item.quality = decreaseQuality(item.quality)
           }
           if (item.sellIn < 0) {
             if (item.quality > 0) {
-              item.quality -= 1
+              item.quality = decreaseQuality(item.quality)
             }
           }
         }
 
         if (item.name.equals("Conjured Mana Cake")) {
           if (item.quality > 0) {
-            item.quality -= 1
+            item.quality = decreaseQuality(item.quality)
           }
           if (item.sellIn < 0) {
             if (item.quality > 0) {
-              item.quality -= 1
+              item.quality = decreaseQuality(item.quality)
             }
           }
         }
 
         if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
           if (item.quality < 50) {
-            item.quality += 1
+            item.quality = increaseQuality(item.quality)
             if (item.quality < 50) {
               if (item.sellIn < 10) {
-                item.quality += 1
+                item.quality = increaseQuality(item.quality)
               }
               if (item.sellIn < 5) {
-                item.quality += 1
+                item.quality = increaseQuality(item.quality)
               }
             }
           }
@@ -69,11 +108,11 @@ class GildedRose(val items: Array[Item]) {
 
         if (item.name.equals("Aged Brie")) {
           if (item.quality < 50) {
-            item.quality += 1
+            item.quality = increaseQuality(item.quality)
           }
           if (item.sellIn < 0) {
             if (item.quality < 50) {
-              item.quality += 1
+              item.quality = increaseQuality(item.quality)
             }
           }
         }
