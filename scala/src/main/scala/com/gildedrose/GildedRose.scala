f89db50d04eb677c536a81313d27400e15df74eb
robin <robin.cavalieri@businessdecision.be>
2019-10-14 15:44:28 +0200

    leave before meeting

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -9,9 +9,9 @@ class GildedRose(val items: Array[Item]) {
    * @param quality
    * @return
    */
-  def increaseQuality(quality: Int): Int = {
+  def increaseQuality(quality: Int, times: Int): Int = {
     if (quality < 50) {
-      quality + 1
+      quality + times
     }
     else quality
   }
@@ -21,8 +21,11 @@ class GildedRose(val items: Array[Item]) {
    * @param quality
    * @return
    */
-  def decreaseQuality(quality: Int): Int = {
-    quality - 1
+  def decreaseQuality(quality: Int, times: Int): Int = {
+    if (quality > 0) {
+      quality - times
+    }
+    else quality
   }
 
   /**
@@ -30,8 +33,8 @@ class GildedRose(val items: Array[Item]) {
    * @param sellIn
    * @return
    */
-  def increaseSellIn(sellIn: Int): Int = {
-    sellIn + 1
+  def increaseSellIn(sellIn: Int, times: Int): Int = {
+    sellIn + times
   }
 
   /**
@@ -39,8 +42,32 @@ class GildedRose(val items: Array[Item]) {
    * @param sellIn
    * @return
    */
-  def decreaseSellIn(sellIn: Int): Int = {
-    sellIn - 1
+  def decreaseSellIn(sellIn: Int, times: Int): Int = {
+    sellIn - times
+  }
+
+  /**
+   *
+   * @param item
+   */
+  def updateDexterity(item: Item): Unit = {
+    item.sellIn = decreaseSellIn(item.sellIn, 1)
+    if (item.sellIn < 0) {
+      item.quality = decreaseQuality(item.quality, 2)
+    }
+    else {
+      item.quality = decreaseQuality(item.quality, 1)
+    }
+  }
+
+  def updateElixir(item: Item): Unit = {
+    item.sellIn = decreaseSellIn(item.sellIn, 1)
+    if (item.sellIn < 0) {
+      item.quality = decreaseQuality(item.quality, 2)
+    }
+    else {
+      item.quality = decreaseQuality(item.quality, 1)
+    }
   }
 
   /**
@@ -54,79 +81,47 @@ class GildedRose(val items: Array[Item]) {
       if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
         print("Sulfuras, Hand of Ragnaros has no action")
       }
-      else {
 
-        item.sellIn = decreaseSellIn(item.sellIn)
-
-
-        // ITEM QUALITY MANAGEMENT
-        if (item.name.equals("+5 Dexterity Vest")) {
-
-          if (item.quality > 0) {
-            item.quality = decreaseQuality(item.quality)
-          }
-          
-          if (item.quality > 0 && item.sellIn < 0) {
-            item.quality = decreaseQuality(item.quality)
-          }
-
-        }
-
-
-        if (item.name.equals("Elixir of the Mongoose")) {
-
-          if (item.quality > 0) {
-            item.quality = decreaseQuality(item.quality)
-          }
-
-          if (item.quality > 0 && item.sellIn < 0) {
-            item.quality = decreaseQuality(item.quality)
-          }
-
-        }
-
-
-        if (item.name.equals("Conjured Mana Cake")) {
-
-          if (item.quality > 0) {
-            item.quality = decreaseQuality(item.quality)
-          }
-
-          if (item.quality > 0 && item.sellIn < 0) {
-              item.quality = decreaseQuality(item.quality)
-          }
-
-        }
-
-
-        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-          item.quality = increaseQuality(item.quality)
-
-          if (item.sellIn < 10) {
-            item.quality = increaseQuality(item.quality)
-          }
-          if (item.sellIn < 5) {
-            item.quality = increaseQuality(item.quality)
-          }
-          if (item.sellIn < 0) {
-            item.quality = 0
-          }
-
-        }
-
-
-        if (item.name.equals("Aged Brie")) {
-
-          item.quality = increaseQuality(item.quality)
-
-          if (item.sellIn < 0) {
-            item.quality = increaseQuality(item.quality)
-          }
-
-        }
-
-
+      else if (item.name.equals("+5 Dexterity Vest")) {
+        updateDexterity(item)
       }
+
+      else if (item.name.equals("Elixir of the Mongoose")) {
+        updateElixir(item)
+      }
+
+      else if (item.name.equals("Conjured Mana Cake")) {
+        item.sellIn = decreaseSellIn(item.sellIn, 1)
+        if (item.sellIn < 0) {
+          item.quality = decreaseQuality(item.quality, 3)
+        }
+        else {
+          item.quality = decreaseQuality(item.quality, 1)
+        }
+      }
+
+      else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        item.sellIn = decreaseSellIn(item.sellIn, 1)
+        item.quality = increaseQuality(item.quality, 1)
+        if (item.sellIn >= 5 && item.sellIn < 10) {
+          item.quality = increaseQuality(item.quality, 1)
+        }
+        else if (item.sellIn >= 0 && item.sellIn < 5) {
+          item.quality = increaseQuality(item.quality, 2)
+        }
+        else if (item.sellIn < 0) {
+          item.quality = 0
+        }
+      }
+
+      else if (item.name.equals("Aged Brie")) {
+        item.sellIn = decreaseSellIn(item.sellIn, 1)
+        item.quality = increaseQuality(item.quality, 1)
+        if (item.sellIn < 0) {
+          item.quality = increaseQuality(item.quality, 1)
+        }
+      }
+      
     }
   }
 }
