623f95f998ec57cafbe18904b34298f6884254c8
robin <robin.cavalieri@businessdecision.be>
2019-10-14 12:54:40 +0200

    Change -= +=

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -3,51 +3,51 @@ package com.gildedrose
 class GildedRose(val items: Array[Item]) {
 
   def updateQuality() {
-    for (obj <- items) {
-      if (!obj.name.equals("Aged Brie")  && !obj.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-        if (obj.quality > 0) {
-          if (!obj.name.equals("Sulfuras, Hand of Ragnaros")) {
-            obj.quality = obj.quality - 1
+    for (item <- items) {
+      if (item.name.equals("Sulfuras, Hand of Ragnaros")  || item.name.equals("+5 Dexterity Vest") || item.name.equals("Elixir of the Mongoose") || item.name.equals("Conjured Mana Cake")) {
+        if (item.quality > 0) {
+          if (item.name.equals("+5 Dexterity Vest") || item.name.equals("Elixir of the Mongoose") || item.name.equals("Conjured Mana Cake")) {
+            item.quality-= 1
           }
         }
       } else {
-        if (obj.quality < 50) {
-          obj.quality = obj.quality + 1
+        if (item.quality < 50) {
+          item.quality += 1
 
-          if (obj.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (obj.sellIn < 11) {
-              if (obj.quality < 50) {
-                obj.quality = obj.quality + 1
+          if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.sellIn < 11) {
+              if (item.quality < 50) {
+                item.quality += 1
               }
             }
 
-            if (obj.sellIn < 6) {
-              if (obj.quality < 50) {
-                obj.quality = obj.quality + 1
+            if (item.sellIn < 6) {
+              if (item.quality < 50) {
+                item.quality += 1
               }
             }
           }
         }
       }
 
-      if (!obj.name.equals("Sulfuras, Hand of Ragnaros")) {
-        obj.sellIn = obj.sellIn - 1
+      if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+        item.sellIn = item.sellIn - 1
       }
 
-      if (obj.sellIn < 0) {
-        if (!obj.name.equals("Aged Brie")) {
-          if (!obj.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (obj.quality > 0) {
-              if (!obj.name.equals("Sulfuras, Hand of Ragnaros")) {
-                obj.quality = obj.quality - 1
+      if (item.sellIn < 0) {
+        if (!item.name.equals("Aged Brie")) {
+          if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.quality > 0) {
+              if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.quality-= 1
               }
             }
           } else {
-            obj.quality = obj.quality - obj.quality
+            item.quality = item.quality - item.quality
           }
         } else {
-          if (obj.quality < 50) {
-            obj.quality = obj.quality + 1
+          if (item.quality < 50) {
+            item.quality += 1
           }
         }
       }
