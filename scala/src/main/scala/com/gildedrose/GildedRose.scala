ae712e0648ee5aedec0627c62f5b82d3f5068b01
bangiono <brice.angiono@businessdecision.be>
2019-10-14 11:43:52 +0200

    Refactoring

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -2,11 +2,46 @@ package com.gildedrose
 
 class GildedRose(val items: Array[Item]) {
 
+  def increaseItemQuality( item: Item, increaseNumber: Int): Unit ={
+
+    //increase the quality item by the given number
+    item.quality += increaseNumber
+  }
+
+
+  def decreaseItemQuality( item: Item, decreaseNumber: Int): Unit ={
+
+    //decrease the quality item by the given number
+    item.quality -= decreaseNumber
+  }
+
+  def isAgedBrie( item: Item): Boolean ={
+
+    item.name == "Aged Brie"
+
+  }
+
+  def isBackstage( item: Item): Boolean ={
+
+    item.name == "Backstage passes to a TAFKAL80ETC concert"
+  }
+
+  def isLegendary( item: Item): Boolean ={
+
+    item.name == "Sulfuras, Hand of Ragnaros"
+  }
+
+  def checkItemQuality(index: Int, qualityNumber: Int): Unit ={
+
+  }
+
+
 
   def updateQuality() {
     for (i <- 0 until items.length) {
-      if (!items(i).name.equals("Aged Brie")
-        && !items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+
+      if (!isAgedBrie(items(i))
+              && !isBackstage(items(i))) {
         if (items(i).quality > 0) {
           if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
             items(i).quality = items(i).quality - 1
--- scala/src/test/scala/com/gildedrose/GildedRoseTest.scala
+++ scala/src/test/scala/com/gildedrose/GildedRoseTest.scala
@@ -49,16 +49,261 @@ class GildedRoseTest  {
      // new Item("Conjured Mana Cake", 2, 5)
 
     )
-    //assertEquals(true , true)
-    //assertTrue(app.items(0).name == "+5 Dexterity Vest")
-    println(app.items(0).quality)
-   // assertTrue(app.items(0).quality == 20)
     app.updateQuality()
 
     assertEquals(expectedItems.toList, app.items.toList)
 
 
 
+
+  }
+
+  @Test
+  def testAfterTwoDays(): Unit ={
+
+
+    var expectedItems = Array[Item](
+      new Item("+5 Dexterity Vest", 8, 18),
+      new Item("Aged Brie", 0, 2),
+      new Item("Elixir of the Mongoose", 3, 5),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 13, 22),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 8, 50),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 3, 50)
+      // this conjured item does not work properly yet
+      // new Item("Conjured Mana Cake", 2, 5)
+
+    )
+    for (i <- 0 until 2)
+    {
+      app.updateQuality()
+    }
+    assertEquals(expectedItems.toList, app.items.toList)
+
+  }
+
+  @Test
+  def testAfterThreeDays(): Unit ={
+
+
+    var expectedItems = Array[Item](
+      new Item("+5 Dexterity Vest", 7, 17),
+      new Item("Aged Brie", -1, 4),
+      new Item("Elixir of the Mongoose", 2, 4),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 12, 23),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 7, 50),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 2, 50)
+      // this conjured item does not work properly yet
+      // new Item("Conjured Mana Cake", 2, 5)
+
+    )
+
+    for (i <- 0 until 3)
+    {
+      app.updateQuality()
+
+    }
+
+    assertEquals(expectedItems.toList, app.items.toList)
+
+  }
+
+  @Test
+  def testAfterFourDays(): Unit ={
+
+
+    var expectedItems = Array[Item](
+      new Item("+5 Dexterity Vest", 6, 16),
+      new Item("Aged Brie", -2, 6),
+      new Item("Elixir of the Mongoose", 1, 3),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 11, 24),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 6, 50),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 1, 50)
+      // this conjured item does not work properly yet
+      // new Item("Conjured Mana Cake", 2, 5)
+
+    )
+
+    for (i <- 0 until 4)
+    {
+      app.updateQuality()
+
+    }
+
+    assertEquals(expectedItems.toList, app.items.toList)
+
+  }
+
+  @Test
+  def testAfterFiveDays(): Unit ={
+
+
+    var expectedItems = Array[Item](
+      new Item("+5 Dexterity Vest", 5, 15),
+      new Item("Aged Brie", -3, 8),
+      new Item("Elixir of the Mongoose", 0, 2),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 25),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 50),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 0, 50)
+      // this conjured item does not work properly yet
+      // new Item("Conjured Mana Cake", 2, 5)
+
+    )
+
+    for (i <- 0 until 5)
+    {
+      app.updateQuality()
+
+    }
+
+    assertEquals(expectedItems.toList, app.items.toList)
+
+  }
+
+  @Test
+  def testAfterSixDays(): Unit ={
+
+
+    var expectedItems = Array[Item](
+      new Item("+5 Dexterity Vest", 4, 14),
+      new Item("Aged Brie", -4, 10),
+      new Item("Elixir of the Mongoose", -1, 0),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 9, 27),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 4, 50),
+      new Item("Backstage passes to a TAFKAL80ETC concert", -1, 0)
+      // this conjured item does not work properly yet
+      // new Item("Conjured Mana Cake", 2, 5)
+
+    )
+
+    for (i <- 0 until 6)
+    {
+      app.updateQuality()
+
+    }
+
+    assertEquals(expectedItems.toList, app.items.toList)
+
+  }
+
+  @Test
+  def testAfterSevenDays(): Unit ={
+
+
+    var expectedItems = Array[Item](
+      new Item("+5 Dexterity Vest", 3, 13),
+      new Item("Aged Brie", -5, 12),
+      new Item("Elixir of the Mongoose", -2, 0),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 8, 29),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 3, 50),
+      new Item("Backstage passes to a TAFKAL80ETC concert", -2, 0)
+      // this conjured item does not work properly yet
+      // new Item("Conjured Mana Cake", 2, 5)
+
+    )
+
+    for (i <- 0 until 7)
+    {
+      app.updateQuality()
+
+    }
+
+    assertEquals(expectedItems.toList, app.items.toList)
+
+  }
+
+  @Test
+  def testAfterEightDays(): Unit ={
+
+
+    var expectedItems = Array[Item](
+      new Item("+5 Dexterity Vest", 2, 12),
+      new Item("Aged Brie", -6, 14),
+      new Item("Elixir of the Mongoose", -3, 0),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 7, 31),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 2, 50),
+      new Item("Backstage passes to a TAFKAL80ETC concert", -3, 0)
+      // this conjured item does not work properly yet
+      // new Item("Conjured Mana Cake", 2, 5)
+
+    )
+
+    for (i <- 0 until 8)
+    {
+      app.updateQuality()
+
+    }
+
+    assertEquals(expectedItems.toList, app.items.toList)
+
+  }
+
+  @Test
+  def testAfterNineDays(): Unit ={
+
+
+    var expectedItems = Array[Item](
+      new Item("+5 Dexterity Vest", 1, 11),
+      new Item("Aged Brie", -7, 16),
+      new Item("Elixir of the Mongoose", -4, 0),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 6, 33),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 1, 50),
+      new Item("Backstage passes to a TAFKAL80ETC concert", -4, 0)
+      // this conjured item does not work properly yet
+      // new Item("Conjured Mana Cake", 2, 5)
+
+    )
+
+    for (i <- 0 until 9)
+    {
+      app.updateQuality()
+
+    }
+
+    assertEquals(expectedItems.toList, app.items.toList)
+
+  }
+
+  @Test
+  def testAfterTenDays(): Unit ={
+
+
+    var expectedItems = Array[Item](
+      new Item("+5 Dexterity Vest", 0, 10),
+      new Item("Aged Brie", -8, 18),
+      new Item("Elixir of the Mongoose", -5, 0),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 35),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 0, 50),
+      new Item("Backstage passes to a TAFKAL80ETC concert", -5, 0)
+      // this conjured item does not work properly yet
+      // new Item("Conjured Mana Cake", 2, 5)
+
+    )
+
+    for (i <- 0 until 10)
+    {
+      app.updateQuality()
+
+    }
+
+    assertEquals(expectedItems.toList, app.items.toList)
 
   }
 
