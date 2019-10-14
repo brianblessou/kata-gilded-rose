c1eadb124530af6e1a4891ac35b683da54b26fc8
David <dpietraniuk@gmail.com>
2019-10-14 16:26:26 +0200

    commit2

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -4,43 +4,50 @@ class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
-
+    @staticmethod
+    def _decrease_quality(item):
+        """
+        Helper function to increase the quality.
+        :param item: item which we want tod descrease
+        :return: integer, containing the quality minus one
+        """
+        if item.quality > 0:
+            return item.quality - 1
+        else:
+            return item.quality
+    @staticmethod
+    def _increase_quality(item):
+        """
+        Helper function to increase the quality.
+        :param item: item which we want to increase
+        :return: integer, containing the quality plus one
+        """
+        if item.quality < 50:
+            return item.quality + 1
+        else:
+            return item.quality
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                pass
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
+
+                if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+                            item.quality = self._decrease_quality(item)
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                        item.quality = self._increase_quality(item)
+                        if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                            if item.sell_in < 11:
+                                    item.quality = self._increase_quality(item)
+                            if item.sell_in < 6:
+                                    item.quality = self._increase_quality(item)
+                item.sell_in = item.sell_in - 1
+                if item.sell_in < 0:
+                    if item.name != "Aged Brie":
+                        if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                                    item.quality = self._decrease_quality(item)
+                        elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                            item.quality = item.quality - item.quality
+                    else:
+                            item.quality = self._increase_quality(item)
 
-
-class Item:
-    def __init__(self, name, sell_in, quality):
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
-
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,14 +1,92 @@
 # -*- coding: utf-8 -*-
 import unittest
-
-from gilded_rose import Item, GildedRose
-
+from gilded_rose import GildedRose
+from item import Item
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
+    def __init__(self, initparam):
+        super().__init__(initparam)
+        self.items_day_zero = [
+            Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+            Item(name="Aged Brie", sell_in=2, quality=0),
+            Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+            Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
+        ]
+    def update_quality_wrapper(self,day,gilded_rose):
+        for i in range(0,day):
+            gilded_rose.update_quality()
+    def test_day_one(self):
+        gilded_rose = GildedRose(self.items_day_zero)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        actual = gilded_rose.items
+        expected = [
+            Item(name="+5 Dexterity Vest", sell_in=9, quality=19),
+            Item(name="Aged Brie", sell_in=1, quality=1),
+            Item(name="Elixir of the Mongoose", sell_in=4, quality=6),
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=14, quality=21),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=9, quality=50),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=4, quality=50),
+            Item(name="Conjured Mana Cake", sell_in=2, quality=5),
+        ]
+        self.assertCountEqual(actual,expected)
 
+    def test_day_two(self):
+        gilded_rose = GildedRose(self.items_day_zero)
+        self.update_quality_wrapper(2,gilded_rose)
+        actual = gilded_rose.items
+        expected = [
+            Item(name="+5 Dexterity Vest", sell_in=8, quality=18),
+            Item(name="Aged Brie", sell_in=0, quality=2),
+            Item(name="Elixir of the Mongoose", sell_in=3, quality=5),
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=13, quality=22),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=8, quality=50),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=3, quality=50),
+            Item(name="Conjured Mana Cake", sell_in=1, quality=4),
+        ]
+        self.assertCountEqual(actual,expected)
+    def test_day_ten(self):
+        gilded_rose = GildedRose(self.items_day_zero)
+        self.update_quality_wrapper(10,gilded_rose)
+        actual = gilded_rose.items
+        expected = [
+            Item(name="+5 Dexterity Vest", sell_in=0, quality=10),
+            Item(name="Aged Brie", sell_in=-8, quality=18),
+            Item(name="Elixir of the Mongoose", sell_in=-5, quality=0),
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=35),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=0, quality=50),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=-5, quality=0),
+            Item(name="Conjured Mana Cake", sell_in=-7, quality=0),
+        ]
+        self.assertCountEqual(actual,expected)
+
+    def test_day_twenty(self):
+        gilded_rose = GildedRose(self.items_day_zero)
+        self.update_quality_wrapper(20,gilded_rose)
+        actual = gilded_rose.items
+        expected = [
+            Item(name="+5 Dexterity Vest", sell_in=-10, quality=0),
+            Item(name="Aged Brie", sell_in=-18, quality=38),
+            Item(name="Elixir of the Mongoose", sell_in=-15, quality=0),
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=-5, quality=0),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=-10, quality=0),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=-15, quality=0),
+            Item(name="Conjured Mana Cake", sell_in=-17, quality=0),
+        ]
+        self.assertCountEqual(actual,expected)
 if __name__ == '__main__':
     unittest.main()
+
+
+
