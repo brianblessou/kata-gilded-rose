10c59f06345d3a977486b62e672209e558b8563e
robin <robin.cavalieri@businessdecision.be>
2019-10-14 13:49:28 +0200

    Fixing break issue

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -5,55 +5,53 @@ import util.control.Breaks._
 class GildedRose(val items: Array[Item]) {
 
   def updateQuality() {
+
     for (item <- items) {
 
       // DO NOTHING
-      if (item.name.equals("Sulfuras, Hand of Ragnaros")) ()
-
-      if (item.name.equals("+5 Dexterity Vest") || item.name.equals("Elixir of the Mongoose") || item.name.equals("Conjured Mana Cake")) {
-        if (item.quality > 0) {
-          if (item.name.equals("+5 Dexterity Vest") || item.name.equals("Elixir of the Mongoose") || item.name.equals("Conjured Mana Cake")) {
-            item.quality-= 1
+      if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+        print("Sulfuras, Hand of Ragnaros has no action")
+      }
+      else {
+        if (item.name.equals("+5 Dexterity Vest") || item.name.equals("Elixir of the Mongoose") || item.name.equals("Conjured Mana Cake")) {
+          if (item.quality > 0) {
+            item.quality -= 1
           }
         }
-      } else {
-        if (item.quality < 50) {
-          item.quality += 1
-
-          if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality += 1
-              }
-            }
-
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality += 1
-              }
-            }
-          }
-        }
-      }
-
-      if (item.name.equals("Aged Brie") || item.name.equals("+5 Dexterity Vest") || item.name.equals("Elixir of the Mongoose") || item.name.equals("Conjured Mana Cake") || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-        item.sellIn -= 1
-      }
-
-      if (item.sellIn < 0) {
-        if (!item.name.equals("Aged Brie")) {
-          if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0) {
-              if (item.name.equals("+5 Dexterity Vest") || item.name.equals("Elixir of the Mongoose") || item.name.equals("Conjured Mana Cake")) {
-                item.quality -= 1
-              }
-            }
-          } else {
-            item.quality = 0
-          }
-        } else {
+        else {
           if (item.quality < 50) {
             item.quality += 1
+
+            if (item.quality < 50) {
+
+              if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.sellIn < 11) {
+                  item.quality += 1
+                }
+                if (item.sellIn < 6) {
+                  item.quality += 1
+                }
+              }
+
+            }
+          }
+        }
+        item.sellIn -= 1
+        if (item.sellIn < 0) {
+          if (item.name.equals("+5 Dexterity Vest") || item.name.equals("Elixir of the Mongoose") || item.name.equals("Conjured Mana Cake") || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.name.equals("+5 Dexterity Vest") || item.name.equals("Elixir of the Mongoose") || item.name.equals("Conjured Mana Cake")) {
+              if (item.quality > 0) {
+                if (item.name.equals("+5 Dexterity Vest") || item.name.equals("Elixir of the Mongoose") || item.name.equals("Conjured Mana Cake")) {
+                  item.quality -= 1
+                }
+              }
+            } else {
+              item.quality = 0
+            }
+          } else {
+            if (item.quality < 50) {
+              item.quality += 1
+            }
           }
         }
       }
