e7705a8481e5a60cb7d42faa1f68661ac6327434
robin <robin.cavalieri@businessdecision.be>
2019-10-14 13:19:06 +0200

    change calculations

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -1,10 +1,16 @@
 package com.gildedrose
 
+import util.control.Breaks._
+
 class GildedRose(val items: Array[Item]) {
 
   def updateQuality() {
     for (item <- items) {
-      if (item.name.equals("Sulfuras, Hand of Ragnaros")  || item.name.equals("+5 Dexterity Vest") || item.name.equals("Elixir of the Mongoose") || item.name.equals("Conjured Mana Cake")) {
+
+      // DO NOTHING
+      if (item.name.equals("Sulfuras, Hand of Ragnaros")) ()
+
+      if (item.name.equals("+5 Dexterity Vest") || item.name.equals("Elixir of the Mongoose") || item.name.equals("Conjured Mana Cake")) {
         if (item.quality > 0) {
           if (item.name.equals("+5 Dexterity Vest") || item.name.equals("Elixir of the Mongoose") || item.name.equals("Conjured Mana Cake")) {
             item.quality-= 1
@@ -30,20 +36,20 @@ class GildedRose(val items: Array[Item]) {
         }
       }
 
-      if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-        item.sellIn = item.sellIn - 1
+      if (item.name.equals("Aged Brie") || item.name.equals("+5 Dexterity Vest") || item.name.equals("Elixir of the Mongoose") || item.name.equals("Conjured Mana Cake") || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        item.sellIn -= 1
       }
 
       if (item.sellIn < 0) {
         if (!item.name.equals("Aged Brie")) {
           if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (item.quality > 0) {
-              if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.quality-= 1
+              if (item.name.equals("+5 Dexterity Vest") || item.name.equals("Elixir of the Mongoose") || item.name.equals("Conjured Mana Cake")) {
+                item.quality -= 1
               }
             }
           } else {
-            item.quality = item.quality - item.quality
+            item.quality = 0
           }
         } else {
           if (item.quality < 50) {
