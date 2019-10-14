4737a432a2ba757903d67ea54efe073d7ff6f93c
David <dpietraniuk@gmail.com>
2019-10-14 15:17:32 +0200

    new commit

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -22,10 +22,10 @@ class GildedRose(object):
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
                         if item.sell_in < 11:
                             if item.quality < 50:
-                                item.quality = item.quality + 1
+                                item.quality = self.quality_add_1(item)
                         if item.sell_in < 6:
                             if item.quality < 50:
-                                item.quality = item.quality + 1
+                                item.quality = self.sell_in_add_1(item)
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
