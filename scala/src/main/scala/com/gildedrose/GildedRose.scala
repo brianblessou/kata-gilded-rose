e404472b68867341568e1195f596c03ea072c768
bangiono <brice.angiono@businessdecision.be>
2019-10-14 14:58:00 +0200

    Refactoring first part + Legendary

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -41,57 +41,69 @@ class GildedRose(val items: Array[Item]) {
 
 
   def updateQuality() {
-    items.foreach{item  =>
+    items.foreach { item =>
 
-      if (!isAgedBrie(item)
-        && !isBackstage(item)) {
-        if (item.quality > 0) {
-          if (!isLegendary(item)) {
-            decreaseItemQuality(item,1)
-          }
-        }
-      } else {
-        if (item.quality < 50) {
-          increaseItemQuality(item,1)
 
-          if (isBackstage(item)) {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                increaseItemQuality(item,1)
-              }
-            }
-
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                increaseItemQuality(item,1)
-              }
-            }
-          }
-        }
+      if (isLegendary(item)) {
+        print(" ")
       }
-
-      if (!isLegendary(item)) {
-        item.sellIn = item.sellIn - 1
-      }
-
-      if (item.sellIn < 0) {
-        if (!isAgedBrie(item)) {
-          if (!isBackstage(item)) {
-            if (item.quality > 0) {
-              if (!isLegendary(item)) {
-                decreaseItemQuality(item,1)
-              }
-            }
-          } else {
-            item.quality = item.quality - item.quality
-          }
-        } else {
+      else {
+        if (isAgedBrie(item)
+          || isBackstage(item)) {
           if (item.quality < 50) {
-            increaseItemQuality(item,1)
+            increaseItemQuality(item, 1)
+
+            if (isBackstage(item)) {
+              if (item.quality < 50) {
+                if (item.sellIn < 11) {
+                  increaseItemQuality(item, 1)
+                }
+
+                if (item.sellIn < 6) {
+                  increaseItemQuality(item, 2)
+                }
+                if (item.sellIn < 0) {
+                  item.quality = item.quality - item.quality
+                }
+
+              }
+            }
+          }
+
+        }
+
+        else {
+
+
+          if (item.quality > 0) {
+            decreaseItemQuality(item, 1)
+          }
+
+        }
+          item.sellIn = item.sellIn - 1
+        if (item.sellIn < 0) {
+
+          if (!isAgedBrie(item)) {
+            if (!isBackstage(item)) {
+              if (item.quality > 0) {
+
+                  decreaseItemQuality(item, 1)
+
+              }
+            }
+
+            else {
+              item.quality = item.quality - item.quality
+            }
+          }
+
+          else {
+            if (item.quality < 50) {
+              increaseItemQuality(item, 1)
+            }
           }
         }
       }
     }
-
   }
 }
