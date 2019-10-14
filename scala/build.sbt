0051bfc78055180e304bfce2fb63a5ab7d8d044b
robin <robin.cavalieri@businessdecision.be>
2019-10-14 10:52:49 +0200

    Tests in place

--- scala/build.sbt
+++ scala/build.sbt
@@ -4,4 +4,4 @@ version := "1.0"
 
 scalaVersion := "2.12.1"
 
-libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.1"
+libraryDependencies += "junit" % "junit" % "4.12" % Test
--- scala/src/test/scala/com/gildedrose/GildedRoseTest.scala
+++ scala/src/test/scala/com/gildedrose/GildedRoseTest.scala
@@ -1,12 +1,59 @@
 package com.gildedrose
 
-import org.scalatest._
+import org.junit.Assert.assertEquals
+import com.gildedrose
+import org.junit.Test
 
-class GildedRoseTest  extends FlatSpec with Matchers {
-      it should "foo" in {
-        var items = Array[Item](new Item("foo", 0, 0))
-        val app = new GildedRose(items)
-        app.updateQuality()
-        (app.items(0).name) should equal ("fixme")
-      }
+class GildedRoseTest {
+
+  val items = Array[Item](
+    new Item("+5 Dexterity Vest", 10, 20),
+    new Item("Aged Brie", 2, 0),
+    new Item("Elixir of the Mongoose", 5, 7)
+  )
+
+  val app = new GildedRose(items)
+
+  @Test
+  def testAfterOneDay() = {
+    val itemsExpected = Array[Item](
+      new Item("+5 Dexterity Vest", 9, 19),
+      new Item("Aged Brie", 1, 1),
+      new Item("Elixir of the Mongoose", 4, 6)
+    )
+    app.updateQuality()
+    assertEquals(itemsExpected.toList, app.items.toList)
+  }
+
+  @Test
+  def testAfterTwoDays() = {
+    val itemsExpected = Array[Item](
+      new Item("+5 Dexterity Vest", 8, 18),
+      new Item("Aged Brie", 0, 2),
+      new Item("Elixir of the Mongoose", 3, 5)
+    )
+
+    for (i <- 1 to 2) {
+      app.updateQuality()
+    }
+
+    assertEquals(itemsExpected.toList, app.items.toList)
+  }
+
+  @Test
+  def testAfterThreeDays() = {
+    val itemsExpected = Array[Item](
+      new Item("+5 Dexterity Vest", 7, 17),
+      new Item("Aged Brie", -1, 4),
+      new Item("Elixir of the Mongoose", 2, 4)
+    )
+
+    for (i <- 1 to 3) {
+      app.updateQuality()
+    }
+
+    assertEquals(itemsExpected.toList, app.items.toList)
+  }
+
+
 }
