b7b64fac5ec0328a3621f8d708742e3c6cab1e57
David <dpietraniuk@gmail.com>
2019-10-15 10:32:16 +0200

    commit3

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -29,22 +29,23 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             if item.name == "Sulfuras, Hand of Ragnaros":
-                pass
-            else:
-                if item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert":
+                continue
+            if item.name=="Aged Brie":
+                item.quality = self._increase_quality(item)
+            elif item.name=="Backstage passes to a TAFKAL80ETC concert":
+                item.quality = self._increase_quality(item)
+                if item.sell_in < 11:
                     item.quality = self._increase_quality(item)
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                                item.quality = self._increase_quality(item)
+            else:
+                item.quality = self._decrease_quality(item)
+
+
+            item.sell_in = item.sell_in - 1
+            if item.sell_in < 0:
+                if item.name == "Aged Brie":
+                    item.quality = self._increase_quality(item)
+                elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                        item.quality = item.quality - item.quality
                 else:
                     item.quality = self._decrease_quality(item)
-
-                item.sell_in = item.sell_in - 1
-                if item.sell_in < 0:
-                    if item.name == "Aged Brie":
-                        item.quality = self._increase_quality(item)
-                    elif item.name == "Backstage passes to a TAFKAL80ETC concert":
-                            item.quality = item.quality - item.quality
-                    else:
-                        item.quality = self._decrease_quality(item)
 
